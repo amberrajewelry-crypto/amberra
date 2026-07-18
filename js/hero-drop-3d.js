a4cc5e71c823d3885369e93dd1b696d676b995c4
Vladimir <amberrajewelry@gmail.com>
2026-07-18 21:26:45 +0400

    feat(hero): капля ниже (центр экрана) + свечение (эмиссия #ffab3c + пульсирующий золотой ореол)

--- js/hero-drop-3d.js
+++ js/hero-drop-3d.js
@@ -57,6 +57,7 @@ function init() {
     roughness: 0.38, envMapIntensity: 0.8,
     clearcoat: 0.25, clearcoatRoughness: 0.4, sheen: 0.45,
     sheenColor: new THREE.Color(0xffd968), sheenRoughness: 0.6,
+    emissive: new THREE.Color(0xffab3c), emissiveIntensity: 0.32,
   });
   const sparkMat = new THREE.MeshStandardMaterial({
     color: 0xf3d488, emissive: 0xd8a94a, emissiveIntensity: 0.5, roughness: 0.6, metalness: 0.7,
